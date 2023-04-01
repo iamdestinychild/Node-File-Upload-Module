fc6c4d8f8a2251ab2d1d8d9237c384b13768f05e
Destiny <omoruyidestiny30@gmail.com>
2023-04-01 13:34:20 +0100

    implement upload route

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,11 @@
 const express = require('express')
 
-const {getProduct, createProduct} = require('../controller/products')
+const {getAllProduct, createProduct} = require('../controller/products')
 const uploadProductImage = require('../controller/upload')
 
 const router = express.Router()
 
-router.route('/').get(getProduct).post(createProduct)
+router.route('/').get(getAllProduct).post(createProduct)
 router.route('/upload').post(uploadProductImage)
 
 module.exports = router
