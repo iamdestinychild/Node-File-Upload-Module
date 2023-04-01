526f35cf559c7890f3a09bee8c13dfb3576bccca
Destiny <omoruyidestiny30@gmail.com>
2023-04-01 14:05:36 +0100

    added checks on upload controller

--- controller/upload.js
+++ controller/upload.js
@@ -1,8 +1,28 @@
 const path = require('path')
 const {StatusCodes} = require('http-status-codes')
+const { BadRequest } = require('../error')
 
 const uploadController = async (req, res) => {
+
+    console.log(req.files)
+
     const productImage = req.files.image
+
+
+    if (!req.files) {
+        throw new BadRequest('No Files Found')
+    }
+
+    if (!productImage.mimetype.startsWith('image')) {
+        throw new BadRequest('Image Not Provided')
+    }
+
+    const maxSize = 1024 * 1024
+
+    if (productImage.size > maxSize) {
+        throw new BadRequest('File can not be greater than 1kb')
+    }
+
 
     const imagePath = path.join(__dirname, `../public/uploads/${productImage.name}`)
 
