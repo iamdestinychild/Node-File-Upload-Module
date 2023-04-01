ddccc017bd30e8d2308d8c963b990634cff9ba0f
Destiny <omoruyidestiny30@gmail.com>
2023-04-01 13:34:39 +0100

    implement upload controller

--- controller/products.js
+++ controller/products.js
@@ -1,14 +1,26 @@
-const product = require('../model/products')
-
-const getProduct = async (req, res) => {
-    res.send('get product')
-}
+const Product = require('../model/products')
+const { BadRequest } = require('../error/index')
+const {StatusCodes} = require('http-status-codes')
 
 const createProduct = async (req, res) => {
-    res.send('create product')
+    const { name, price, image } = req.body
+    
+    if (!name || !price || !image) {
+        throw new BadRequest('Name, Price and image are required fileds')
+    }
+    
+    const product = await Product.create({ name, price, image })
+    
+    res.status(StatusCodes.OK).json({product})
+}
+
+const getAllProduct = async (req, res) => {
+    const product = await Product.find({})
+
+    res.status(StatusCodes.OK).json(product)
 }
 
 module.exports = {
-    getProduct,
+    getAllProduct,
     createProduct
 }
