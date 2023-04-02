3b78ca1e365e178e24246a4ea03cbe10898ab051
Destiny <omoruyidestiny30@gmail.com>
2023-04-02 13:16:54 +0100

    upload to cloudinary script

--- controller/upload.js
+++ controller/upload.js
@@ -1,35 +1,41 @@
 const path = require('path')
+const fs = require('fs')
+const cloudinary = require('cloudinary').v2
 const {StatusCodes} = require('http-status-codes')
 const { BadRequest } = require('../error')
 
 const uploadController = async (req, res) => {
 
-    console.log(req.files)
-
     const productImage = req.files.image
 
 
-    if (!req.files) {
-        throw new BadRequest('No Files Found')
-    }
+    // if (!req.files) {
+    //     throw new BadRequest('No Files Found')
+    // }
 
-    if (!productImage.mimetype.startsWith('image')) {
-        throw new BadRequest('Image Not Provided')
-    }
+    // if (!productImage.mimetype.startsWith('image')) {
+    //     throw new BadRequest('Image Not Provided')
+    // }
 
-    const maxSize = 1024 * 1024
+    // const maxSize = 1024 * 1024
 
-    if (productImage.size > maxSize) {
-        throw new BadRequest('File can not be greater than 1kb')
-    }
+    // if (productImage.size > maxSize) {
+    //     throw new BadRequest('File can not be greater than 1kb')
+    // }
 
 
-    const imagePath = path.join(__dirname, `../public/uploads/${productImage.name}`)
+    
+    const result = await cloudinary.uploader.upload(
+        productImage.tempFilePath,
+        {
+            use_filename: true,
+            folder:'file_upload'
+        }
+    )
 
-    await productImage.mv(imagePath)
-    res.status(StatusCodes.OK).json({
-        image:{src: `../upload/${productImage.name}`}
-    })
+    fs.unlinkSync(productImage.tempFilePath)
+     
+    res.status(StatusCodes.OK).json({image:result.url})
 }
 
 module.exports = uploadController
